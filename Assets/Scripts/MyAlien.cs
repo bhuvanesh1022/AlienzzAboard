88a8747a740c6184d53341d5ffdf5bb746ea0f5f
Bhuvaneshwaran Moorthy <bhuvaneshwaran.m@hornbillfx.com>
2020-11-17 18:05:28 +0530

    Initialized
    initialized and imported LT

--- Assets/Scripts/MyAlien.cs
+++ Assets/Scripts/MyAlien.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyAlien : MonoBehaviour
 {
+    public SpriteRenderer alien;
+    public List<Sprite> faces = new List<Sprite>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,17 @@ public class MyAlien : MonoBehaviour
     {
         
     }
+
+    private void OnMouseDrag()
+    {
+        alien.sprite = faces[1];
+        Debug.Log("Dragging");
+    }
+
+    private void OnMouseUp()
+    {
+        alien.sprite = faces[3];
+    }
+
+    
 }
