149f111982b5518eaa44dd2fcf92b33bb2c21018
Dinesh_MR_X <dinesh.s@hornbillfx.com>
2020-12-10 11:05:01 +0530

    Layout Final

---
+++ Assets/Scripts/OnClickOpen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickOpen : MonoBehaviour
+{
+    public List<GameObject> paneltobeClosed;
+    public GameObject paneltobeActive;
+    public Button btn;
+
+    public int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnOpen()
+    {
+        Debug.Log("clicked");
+        if (count == 0 )
+        {
+            for (int i = 0; i < paneltobeClosed.Count; i++)
+            {
+                paneltobeClosed[i].SetActive(false);
+            }
+            paneltobeActive.SetActive(true);
+            count += 1;
+            Debug.Log("panelopen");
+            return;
+        }
+        if (count == 1)
+        {
+            paneltobeActive.SetActive(false);
+            count = 0;
+            Debug.Log("panelclosed");
+        }
+        
+    }
+}
